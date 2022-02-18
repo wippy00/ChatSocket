5d66f76ac6bb580d63f950f4eff9d287b0cdab53
Wippy00 <lucchini.william@gmail.com>
2022-02-18 09:58:55 +0100

    aua

--- client.py
+++ client.py
@@ -1,36 +1,44 @@
-import socket, threading, re
+import socket
+import threading
+import re
 
 
-HOST = '192.168.1.5'
+USERNAME = ""
+HOST = ''
+PORTA = 0
+FORMAT = "utf-8"
+SIZE = 8096
+#  input('inserisci username: ')
 
-PORTA = 65432
-FORMAT="utf-8"
-SIZE=8096
 
 def msg_send(client):
     while True:
-        msg=input()
-        msg=USERNAME+msg
+        msg = input()
+        msg = USERNAME+msg
         client.send(msg.encode(FORMAT))
+
 
 def msg_recive(client):
     while True:
         try:
-            msg=client.recv(SIZE)
+            msg = client.recv(SIZE)
             if not msg:
-                    break
+                break
             print(msg.decode(FORMAT))
         except:
             print('errore')
             break
+    
 
-USERNAME = input('inserisci username: ')
-USERNAME=USERNAME+': '
-client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST,PORTA))
-client.sendall(USERNAME.encode(FORMAT))
-aua=str(client)
-result = re.search("laddr=(.*)',", au a).group(1)[2:20]
-print('dati connessione: ' + str(result) + '\n')
-threading.Thread(target=msg_recive,args=(client,), daemon=True).start()
-msg_send(client)
+def main():
+    print (USERNAME)
+    print (HOST)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORTA))
+    client.sendall(USERNAME.encode(FORMAT))
+    aua = str(client)
+    result = re.search("laddr=(.*)',", aua).group(1)[2:20]
+    print('dati connessione: ' + str(result) + '\n')
+    threading.Thread(target=msg_recive, args=(client,), daemon=True).start()
+    msg_send(client)
--- interfaccia.py
+++ interfaccia.py
@@ -4,60 +4,100 @@ from tkinter import filedialog
 from tkinter.ttk import Scrollbar
 import client
 
-# def poke(msg):
-#     messagebox.showinfo("You Have Been Poked", msg)
-#     return
 
-# def upload():
-#     root.fileframe = filedialog.askopenfilename(initialdir="/", title="Seleziona un file")
-#     return
+def main_app():
+    
+    global USERNAME
+    USERNAME = entry_username.get()+': '
+    global HOST
+    HOST = entry_host.get()
+    global PORTA
+    PORTA = 65432
 
-def msg_append(msg):
-    text_chat_log.config(state=tk.NORMAL)
-    text_chat_log.insert(tk.END, msg+"\n\n")
+    
+    client.main()
+    init.destroy()
+
+    # def poke(msg):
+    #     messagebox.showinfo("You Have Been Poked", msg)
+    #     return
+
+    # def upload():
+    #     root.fileframe = filedialog.askopenfilename(initialdir="/", title="Seleziona un file")
+    #     return
+
+
+    def msg_append(msg):
+        text_chat_log.config(state=tk.NORMAL)
+        text_chat_log.insert(tk.END, msg+"\n\n")
+        text_chat_log.config(state=tk.DISABLED)
+        text_chat_log.see(tk.END)
+        return
+
+
+    def msg_send():
+        msg = entry_msg_write.get()
+        msg = client.USERNAME+msg
+        msg_append(msg)
+        return
+
+    root = tk.Tk()
+    root.title("aua")
+    # root.iconbitmap("./icon.ico")
+    root.geometry("660x420")
+    #root.resizable(False, False)
+
+    ##############################################################################################
+    #   FRAME   CONTENITORE
+    frame_msg_display = tk.Frame(root, width=30, height=20)
+    frame_msg_display.pack()
+    frame_msg_display.pack_propagate(0)
+    #   TEXT FRAME MESSAGGI
+    text_chat_log = tk.Text(frame_msg_display, bg="white")
+    text_chat_log.grid(row=0, column=0, sticky='ew')
     text_chat_log.config(state=tk.DISABLED)
-    text_chat_log.see(tk.END)
-    return
+    #   SCROLLBAR
+    scrollbar = tk.Scrollbar(frame_msg_display, orient='vertical', command=text_chat_log.yview)
+    scrollbar.grid(row=0, column=1, sticky='ns')
+    text_chat_log['yscrollcommand'] = scrollbar.set
 
-def msg_send():
-    msg=entry_msg_write.get()
-    msg=client.USERNAME+msg
-    msg_append(msg)
-    return
+    ##############################################################################################
+    #   FRAME   CONTENITORE
+    frame_msg_send = tk.Frame(root)
+    frame_msg_send.pack(pady=2)
+    #   ENTRY MESSAGGI
+    entry_msg_write = tk.Entry(frame_msg_send, width=95)
+    entry_msg_write.grid(row=0, column=0, columnspan=3)
+    #   BOTTONE SEND
+    button_msg_send = tk.Button(frame_msg_send, width=10, text="invia", command=msg_send)
+    button_msg_send.grid(row=0, column=4)
 
-root = tk.Tk()
-root.title("aua")
+    root.mainloop()
+
+
+init = tk.Tk()
+init.title("aua")
 # root.iconbitmap("./icon.ico")
-root.geometry("660x420")
+init.geometry("300x100")
 #root.resizable(False, False)
 
-##############################################################################################
-#   FRAME   CONTENITORE
-frame_msg_display = tk.Frame(root, width=30, height=20)
-frame_msg_display.pack()
-frame_msg_display.pack_propagate(0)
-#   TEXT FRAME MESSAGGI
-text_chat_log = tk.Text(frame_msg_display, bg="white")
-text_chat_log.grid(row=0,column=0, sticky='ew')
-text_chat_log.config(state=tk.DISABLED)
-#   SCROLLBAR
-scrollbar = tk.Scrollbar(frame_msg_display, orient='vertical', command=text_chat_log.yview)
-scrollbar.grid(row=0, column=1, sticky='ns')
-text_chat_log['yscrollcommand'] = scrollbar.set
+label_uername = tk.Label(init, text="Username:")
+label_uername.grid(row= 0, column= 0)
+entry_username =tk.Entry(init)
+entry_username.grid(row= 0, column= 1)
 
-##############################################################################################
-#   FRAME   CONTENITORE
-frame_msg_send = tk.Frame(root)
-frame_msg_send.pack(pady=2)
-#   ENTRY MESSAGGI
-entry_msg_write = tk.Entry(frame_msg_send, width=95)
-entry_msg_write.grid(row=0, column=0, columnspan=3)
-#   BOTTONE SEND
-button_msg_send = tk.Button(frame_msg_send, width=10, text="invia", command=msg_send)
-button_msg_send.grid(row=0, column=4)
+label_host = tk.Label(init, text="IP Server:")
+label_host.grid(row= 1, column= 0)
+entry_host =tk.Entry(init)
+entry_host.grid(row= 1, column= 1)
+
+label_port = tk.Label(init, text="Porta:")
+label_port.grid(row= 2, column= 0)
+entry_port =tk.Entry(init)
+entry_port.grid(row= 2, column= 1)
+
+button_connetti = tk.Button(init, width=10, text="invia", command=main_app)
+button_connetti.grid(row= 3, column= 3)
 
 
-
-
-
-root.mainloop()
+init.mainloop()
